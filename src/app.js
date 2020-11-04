d90b89ddd649d861e429cfcdd067f5ae10fa4ef8
Deblapin <71253952+Deblapin@users.noreply.github.com>
2020-11-04 23:12:23 +0200

    updated javascript data

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
-function displayTemperature(response) {console.log(response.data)};
+function displayTemperature(response) {
+    console.log(response.data);
+
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let visibilityElement = document.querySelector("#visibility");
+
+
+
+    temperatureElement.innerHTML= Math.round(response.data.main.temp);
+    cityElement.innerHTML=response.data.name;
+    humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+    visibilityElement.innerHTML=response.data.sys.visibility;
+
+
+
+}
 
 let apiKey = "bae35d99184617162bd406c1c6a7489e";
 let apiUrl =
